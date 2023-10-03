897535a97320766c50c4434ee919339533a179d2
dev_one <micool1030@gmail.com>
2023-10-03 21:36:30 +0900

    upload project

---
+++ src/main/java/me/won/springsecuritynote/SpringSecurityNoteApplication.java
@@ -0,0 +1,13 @@
+package me.won.springsecuritynote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/won/springsecuritynote/SpringSecurityNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package me.won.springsecuritynote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
